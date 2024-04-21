a77540dd15900a7f0c842fc986e12969214da85c
Daniele Dias <daniele.dias.ext@dasa.com.br>
2024-04-21 16:08:51 -0300

    first commit

---
+++ src/pages/Services/index.js
@@ -0,0 +1,17 @@
+import Header from "../../components/Header/Header";
+import "./Services.css";
+
+const Services = () => {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <div className="content">
+          <h1>oi</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Services;
